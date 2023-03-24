db929e3b5d4a0af15808b827231fce2f66903ace
Franklinfzh <franklinfzh@163.com>
2023-03-24 17:29:53 +0800

    3/24

--- text/text.cpp
+++ text/text.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
-float sqr(float a){return a*a;}
-float p(float x,int n){
-    cout<<"in-process: "<<"x="<<x<<'\t'<<"n="<<n<<endl;
-    if(n==0)return 1;
-    else if(n%2!=0 ) return x*sqr(p(x,n/2));
+float sqr(float a) { return a * a; }
+float p(float x, int n)
+{
+    cout << "in-process: "
+         << "x=" << x << '\t' << "n=" << n << endl;
+    if (n == 0)
+        return 1;
+    else if (n % 2 != 0)
+        return x * sqr(p(x, n / 2));
 }
-int main(){
-    cout<<p(2.0,13)<<endl ;
+int main()
+{
+    cout << p(2.0, 13) << endl;
 }
-
+// 写一个斐波那契函数
